e2dfc113a7d59e6722cb585355885b07b9e313ff
Ilya <petrov9846@mail.ru>
2021-11-03 14:52:22 +0300

    create controller

---
+++ src/main/java/com/testcase/testcasegpn/controller/MainController.java
@@ -0,0 +1,29 @@
+package com.testcase.testcasegpn.controller;
+
+import net.minidev.json.JSONObject;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @PostMapping(value = "api/plus", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<JSONObject> plusFunc(HttpEntity<String> arguments){
+        try{
+            System.out.println(arguments.getBody());
+
+            JSONObject response = new JSONObject();
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception ex){
+            ex.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+
+
+    }
+
+}
