ed47e4bc5f9ce539e5e39c76c328d6f45cd454d8
Viktor Belov <sasbyf@gmail.com>
2021-11-03 14:39:37 +0300

    Initial commit

---
+++ src/test/java/com/testcase/testcasegpn/TestcasegpnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcase.testcasegpn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcasegpnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
