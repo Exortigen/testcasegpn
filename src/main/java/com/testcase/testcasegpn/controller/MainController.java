b9b67eeb4529f05d0799961bbf7146031accbe28
Ilya <petrov9846@mail.ru>
2021-11-03 16:17:53 +0300

    добавлена обработка json

--- src/main/java/com/testcase/testcasegpn/controller/MainController.java
+++ src/main/java/com/testcase/testcasegpn/controller/MainController.java
@@ -1,27 +1,51 @@
 package com.testcase.testcasegpn.controller;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
 import net.minidev.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.DataInput;
+import java.io.IOException;
 
 @RestController
 public class MainController {
-    @PostMapping(value = "api/plus", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "api/plus", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<JSONObject> plusFunc(HttpEntity<String> arguments){
-        try{
-            System.out.println(arguments.getBody());
-
-            JSONObject response = new JSONObject();
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } catch (Exception ex){
+        int a=0;
+        int b=0;
+        int result=0;
+        System.out.println(arguments.getBody());
+        try(JsonParser ab = new JsonFactory().createParser(arguments.getBody());){
+        while (ab.nextToken() != JsonToken.END_OBJECT){
+            String fieldname = ab.getCurrentName();
+            if("a".equals(fieldname)){
+                ab.nextToken();
+                a = ab.getValueAsInt();
+            }
+            if("b".equals(fieldname)){
+                ab.nextToken();
+                b = ab.getValueAsInt();
+            }
+        }
+            result=a+b;
+        JSONObject response = new JSONObject();
+        response.put("result",result);
+            return new ResponseEntity<>(response,HttpStatus.OK);
+        } catch (JsonGenerationException ex){
             ex.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-
 
 
     }
