0b4873349c2ed74816ecdc8577eee64ccc87ab8b
Viktor Belov <sasbyf@gmail.com>
2021-11-14 16:49:43 +0300

    MessageService обновил

--- src/main/java/com/testcase/testcasegpn/service/MessageService.java
+++ src/main/java/com/testcase/testcasegpn/service/MessageService.java
@@ -11,38 +11,44 @@ import java.util.Optional;
 public class MessageService {
     @Autowired
     private RequestRepository requestRepository;
+    SoapCallMethod callMethod = new SoapCallMethod();
 
-    public void functionSwitch(Request request, String func){
-        SoapCallMethod callMethod = new SoapCallMethod();
+    private Request functionSwitch(Request request, String func) {
         if (request.getIntA() != 0 && request.getIntB() != 0) {
             switch (func) {
                 case "add":
                     request.setMethodInt(0);
                     request.setMethod("Add");
-                    request.hashCode();
-                    Optional<Request> requestOptional = requestRepository.findByPersonalhash(request.getPersonalhash());
-                    if (requestOptional.isPresent()) {
-                        request = requestOptional.get();
-                    } else {
-                        request.setResult(callMethod.callWeb(request));
-                        requestRepository.save(request);
-                    }
+                    request = checkRequest(request);
                     break;
                 case "divide":
                     request.setMethodInt(1);
                     request.setMethod("Divide");
-                    request.setResult(callMethod.callWeb(request));
+                    request = checkRequest(request);
                     break;
                 case "multiply":
                     request.setMethodInt(3);
                     request.setMethod("Multiply");
-                    request.setResult(callMethod.callWeb(request));
+                    request = checkRequest(request);
                     break;
                 case "subtract":
                     request.setMethodInt(4);
                     request.setMethod("Subtract");
-                    request.setResult(callMethod.callWeb(request));
+                    request = checkRequest(request);
                     break;
             }
+
         }
+    return request;
+    }
+    private Request checkRequest(Request request){
+        Optional<Request> requestOptional = requestRepository.findByPersonalhash(request.getPersonalhash());
+        if (requestOptional.isPresent()) {
+            request = requestOptional.get();
+        } else {
+            request.setResult(callMethod.callWeb(request));
+            requestRepository.save(request);
+        }
+        return request;
+    }
 }
